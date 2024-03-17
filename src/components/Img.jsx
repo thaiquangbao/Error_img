bdbf0590a918f420ed9e279aff9a709ebffbb0fa
Thai Bao <boishiro2002@gmail.com>
2024-03-17 23:57:54 +0700

    Message muốn gửi

---
+++ src/components/Img.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../components/mua-tra-gop-iphone-14-pro-max.jpeg';
+
+const Img = () => {
+    return (
+        <>
+
+            <div className="img-container">
+                <div className="tooltip">
+                    <a  >
+                        <img alt="mua trả góp iphone 14 pro max" style={{ maxWidth: '200px', height: 'auto' }} />
+                    </a>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Img;
